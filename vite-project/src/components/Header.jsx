137f7811ceea8294d41619859a91450963c349c2
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-05-05 04:35:56 +0000

    Added navbar

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import './header.css'
+function Header(){
+    return(
+       <>
+       <div id="navbar">
+        <div id='nav_items'>
+          <ul id="nav_li">
+            <li id="Home">Home</li>
+            <li id="About">About</li>
+            <li id="Products">Products</li>
+          </ul>
+
+        </div>
+      </div>
+       </>
+    )
+ }
+ export default Header
