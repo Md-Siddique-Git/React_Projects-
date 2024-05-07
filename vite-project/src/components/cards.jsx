f20a15649b4247de07949a424d206d896e3637dd
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-05-07 14:33:57 +0000

    added Cards

---
+++ vite-project/src/components/cards.jsx
@@ -0,0 +1,12 @@
+export default function Cards(){
+    return(
+        <>
+        <div className="cards">
+            <div className="card">
+                <h2 style={{color:"white"}} className="title">Boat</h2>
+                <img src="" alt="" />
+            </div>
+        </div>
+        </>
+    )
+}
