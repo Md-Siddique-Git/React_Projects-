3dd6007e1bf0de9688e037e0dbe13f9748053571
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-05-10 15:38:25 +0000

    fixes url

--- vite-project/src/components/cards.jsx
+++ vite-project/src/components/cards.jsx
@@ -4,7 +4,7 @@ export default function Cards(){
         <div className="cards">
             <div className="card">
                 <h2 style={{color:"white"}} className="title">Boat</h2>
-                <img src="" alt="" />
+                <img src="https://images.pexels.com/photos/21602184/pexels-photo-21602184/free-photo-of-a-bicycle-parked-in-front-of-a-tree-in-a-yard.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
             </div>
         </div>
         </>
