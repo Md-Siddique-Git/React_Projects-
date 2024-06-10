9163e79af3023f1951eb6166b9cff7b1ad895943
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-06-10 16:11:43 +0000

    added more features and footer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Cards from "./components/cards"
 import Slider from "./components/sliderBox"
+import Footer from "./components/footer"
 
 
 
@@ -10,12 +11,10 @@ function App() {
     <>
       <Header/>
       <Slider/>
-
-      {/* <div className="justify-center items-center flex"> */}
       <Cards />
+      <Footer/>
 
 
-      {/* </div> */}
       
       
     </>
---
+++ vite-project/src/components/footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return (
+        <>
+        <div className="footer h-20 flex justify-center items-center bg-gray-500">
+            <p className="text-white flex text-center "> &copy;All Rights are reserved </p>
+        </div>
+    
+    
+    
+    </>
+    )
+}
