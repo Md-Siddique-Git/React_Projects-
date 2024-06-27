37a1810d4b3ac85306dab953538eb7fbe0a8e761
MD SIDDIQUE <76256885+Md-Siddique-Git@users.noreply.github.com>
2024-06-27 13:36:09 +0000

    updated image slider

--- vite-project/src/components/footer.jsx
+++ vite-project/src/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return (
         <>
         <div className="footer h-20 flex justify-center items-center bg-gray-500">
-            <p className="text-white flex text-center "> &copy;All Rights are reserved </p>
+            <p className="text-white flex text-center "> &copy;All Rights are reserved by Md Siddique </p>
         </div>
     
     
--- vite-project/src/components/sliderBox.jsx
+++ vite-project/src/components/sliderBox.jsx
@@ -4,13 +4,16 @@ function Slider(){
     return (
         <>
         <div className="flex flex-col items-center  mt-5 ">
-        <div className="h-72 text-center fixed justify-center  rounded-md w-11/12 bg-red-500">hello i am slider box</div>
+        <div className="h-72 text-center justify-center  rounded-md w-11/12 bg-red-500">hello i am slider box</div>
         {
                     data.map((products, i) => (
                         <div key={i}>
-                            <img className="  flex relative justify-center text-center h-24 w-24" src={products.img} alt="image" />
+                            {/* <img className="  flex relative justify-center flex-row text-center h-24 w-24" src={products.img} alt="image" /> */}
                            
                         </div>
+                       
+                        
+                    
                     )
 
                     )
